887dfd90d4352c427e965aa180d121525a4ba32c
Jason <cameljeet@gmail.com>
2016-12-25 14:10:10 -0800

    Create App Component and link it up to the HTML

---
+++ components/App.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+// Components
+import ChannelSection from './channels/ChannelSection.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      channels: [],
+    };
+  }
+
+  addChannel(name){
+    let { channels } = this.state;
+    channels.push({ id: channels.length, name });
+    this.setState({ channels })
+  }
+
+  setChannel(activeChannel) {
+    this.setState({ activeChannel })
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <div className='nav'>
+          <ChannelSection
+            channels={ this.state.channels }
+            addChannel={ this.addChannel.bind(this) }
+            setChannel={ this.setChannel.bind(this) }
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
